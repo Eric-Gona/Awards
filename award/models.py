660453e31c60d0c99d3430ed74590d3508c97c76
Eric-Gona <eric.gona254@gmail.com>
2022-03-15 09:26:01 +0300

    adds classes in models

--- award/models.py
+++ award/models.py
@@ -44,4 +44,34 @@ class Post(models.Model):
     @classmethod
     def search_by_name(cls, search_term):
         got_projects = Post.objects.filter(name__icontains=search_term)
-        return got_projects       
+        return got_projects    
+
+class Rating(models.Model):
+    RATINGS = (
+        (1, '1'),
+        (2, '2'),
+        (3, '3'),
+        (4, '4'),
+        (5, '5'),
+        (6, '6'),
+        (7, '7'),
+        (8, '8'),
+        (9, '9'),
+        (10, '10')
+    )
+    post = models.ForeignKey(Post,on_delete=models.CASCADE)
+    pub_date = models.DateTimeField(auto_now=True)
+    user = models.ForeignKey(User,on_delete=models.CASCADE)
+    usability_rating = models.IntegerField(default=0, choices=RATINGS, null=True)
+    design_rating = models.IntegerField(default=0, choices=RATINGS, null=True)
+    content_rating = models.IntegerField(default=0, choices=RATINGS, null=True)
+    review = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.review
+
+    def save_rating(self):
+        self.save()
+
+    def delete_rating(self):
+        self.delete()

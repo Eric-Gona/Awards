bff5bae4155ad147403dbfe2fa1c233e65b85bcd
Eric-Gona <eric.gona254@gmail.com>
2022-03-15 09:22:42 +0300

    adds imports in models.py

--- award/models.py
+++ award/models.py
@@ -1,3 +1,9 @@
 from django.db import models
-
+from django.contrib.auth.models import User
+from django.db.models.deletion import PROTECT
+from django.dispatch import receiver
+from django.utils import timezone
+from django.db.models.signals import post_save
+from tinymce.models import HTMLField
+from cloudinary.models import CloudinaryField
 # Create your models here.

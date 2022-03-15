b6a3c27944db75a15f82baf7f4bbc07fda128d59
Eric-Gona <eric.gona254@gmail.com>
2022-03-15 09:31:23 +0300

    creats views

--- award/views.py
+++ award/views.py
@@ -7,3 +7,30 @@ from django.db.models import Avg
 from django.contrib.auth.decorators import login_required
 
 # Create your views here.
+def index(request):
+    posts = Post.objects.all()
+    return render(request, 'index.html',{'posts':posts})
+     
+
+
+@login_required(login_url='/accounts/login/')
+def profile(request):
+    posts = Post.objects.all().order_by('-post_date')
+    return render(request, 'profile.html', locals())
+
+
+@login_required(login_url='/accounts/login/')
+def edit(request):
+    if request.method == 'POST':
+        print(request.FILES)
+        new_profile = ProfileForm(
+            request.POST,
+            request.FILES,
+            instance=request.user.profile
+        )
+        if new_profile.is_valid():
+            new_profile.save()
+            return redirect('profile')
+    else:
+        new_profile = ProfileForm(instance=request.user.profile)
+    return render(request, 'edit_profile.html', locals())

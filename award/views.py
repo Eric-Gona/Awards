effe7b4294812e2b08c8ca183466585295fa6be4
Eric-Gona <eric.gona254@gmail.com>
2022-03-15 09:30:18 +0300

    adds viewws.py imports

--- award/views.py
+++ award/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
+from django.contrib.auth.decorators import login_required
+from .models import Post, Rating
+from .forms import ProfileForm, NewPostForm, ProjectRatingForm
+from django.contrib.auth.models import User
+from django.db.models import Avg
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.

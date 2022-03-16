b5b7a661b9bcd44cc5de93de861bdb7f6afae402
Eric-Gona <eric.gona254@gmail.com>
2022-03-16 23:16:39 +0300

    adds changes

---
+++ README.md
@@ -0,0 +1,53 @@
+# PROJECT NAME
+AWARDS
+
+## AUTHOR
+Eric Karisa
+
+## DESCRIPTION
+This project allows users to post their projects for other users to rate according to design, usability and content.
+Behaviour Driven Development
+Users need to Sign in to the application to post projects and review projects
+
+Users can post a project to be rated/viewed.
+
+## User Story
++ Users need to Sign in to the application to post projects and review projects.
+
++ Users can view different projects and their details.
+
++ Users can post a project to be rated/viewed.
+
++ Users can search for different projects.
+
++ Users can view projects overall score.
+
++ Users can view their profile page with all their published projects.
+
++ Users can rate/review other users' projects.
+
+## SETUP/INSTALLATION.
+- Clone this repo: git clone https://github.com/BRIANCHERUIYOT/awads.git.
+
+- The repo comes in a zipped or compressed format. Extract to your prefered location and open it.
+
+- open your terminal and navigate to gallery then create a virtual environment.For detailed guide refer here
+
+- To run the app, you'll have to run the following commands in your terminal
+
+- pip install -r requirements.txt
+- On your terminal,Create database db_name using the command below.
+
+
+## Technologies Used
+- Python 3.8
+- Django
+- Postgresql
+- Bootstrap4
+
+## Contact Information
+If you have any question or contributions, please email me at [eric.karisa@student.moringaschool.com]
+
+
+## License
+licensed under[MIT license](Licensed under MIT license).
--- awards/settings.py
+++ awards/settings.py
@@ -11,6 +11,13 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
+import cloudinary
+import cloudinary.uploader
+import cloudinary.api
+import os
+import django_heroku
+import dj_database_url
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -37,6 +44,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'bootstrap4',
+    'tinymce',
+    'award.apps.AwardConfig',
 ]
 
 MIDDLEWARE = [
@@ -47,6 +57,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'awards.urls'
@@ -76,7 +87,9 @@ WSGI_APPLICATION = 'awards.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': 'award',
+        'USER':'moringa',
+        'PASSWORD':'access',
     }
 }
 
@@ -116,6 +129,24 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
+
+
+STATIC_URL = '/static/'
+
+# Default primary key field type
+# https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+
+# Extra places for collectstatic to find static files.
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+django_heroku.settings(locals())
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field

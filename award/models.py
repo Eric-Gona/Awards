8fb97c3ab87f1abdb6e96c7ef787ef7308faa79a
Eric-Gona <eric.gona254@gmail.com>
2022-03-25 18:25:22 +0300

    deployment

--- award/models.py
+++ award/models.py
@@ -6,6 +6,7 @@ from django.utils import timezone
 from django.db.models.signals import post_save
 from tinymce.models import HTMLField
 from cloudinary.models import CloudinaryField
+
 # Create your models here.
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='profile')
--- award/urls.py
+++ award/urls.py
@@ -2,7 +2,7 @@ from django.urls import path,include, re_path
 from django.conf import settings
 from django.conf.urls.static import static
 from . import views
-from django.conf.urls import url
+from django.urls import re_path as url
 
 urlpatterns= [
     path('', views.index, name='index'),
